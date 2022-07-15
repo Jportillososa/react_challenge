87636b4544fae6a85627dd621b92ee8303c1f1aa
Jose Portillo <jportillososa@MacBook-Pro.attlocal.net>
2022-07-15 14:05:07 -0400

    adding my react route NAV and adding style next

---
+++ src/pages/NoPage.js
@@ -0,0 +1,7 @@
+const NoPage = () => {
+  return <h1>404</h1>;
+};
+
+export default NoPage;
+
+// This page displays if error occurs and content doesnt display on page
---
+++ src/pages/Products.js
@@ -0,0 +1,5 @@
+const Products = () => {
+  return <h1>Here my Products are listed!</h1>;
+};
+
+export default Products;
